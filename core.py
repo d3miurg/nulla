ea10d3689f2ab48071cfededbd2801f400746743
d3miurg <50408380+d3miurg@users.noreply.github.com>
2024-03-13 20:28:00 +0300

    added object-oriented Qt

--- core.py
+++ core.py
@@ -1,14 +1,6 @@
-import sys
-import threading
+import aminofix as amino
 import queue
 import os
-import aminofix as amino
-
-import base64
-import string
-import random
-from hashlib import sha1
-
 import json
 import requests
 
@@ -18,6 +10,7 @@ message_queue = queue.Queue()
 safe_messages = []
 
 user = amino.Client()
+sub_user = None
 
 
 def handle_errors(func):
@@ -38,31 +31,7 @@ def handle_errors(func):
 
 
 @handle_errors
-def compare(message1, message2):
-    compare_index = 0
-    lenght_index = 0
-    if len(message1) > len(message2):
-        short_message = message2
-        long_message = message1
-
-    else:
-        short_message = message1
-        long_message = message2
-
-    for i in short_message:
-        if i == long_message[lenght_index]:
-            compare_index += 1
-
-        lenght_index += 1
-
-    ratio = (compare_index * 100) / lenght_index
-
-    return ratio
-
-
-@handle_errors
 def login(email, password):
-    global user
 
     try:
         user.login(email, password)
@@ -80,45 +49,14 @@ def login(email, password):
     except amino.lib.util.exceptions.InvalidPassword:
         return 'Неверный пароль'
 
-    except amino.lib.util.exceptions.ActionNotAllowed:
-        login_info = open('device.json', 'r')
-        info = json.load(login_info)
-        login_info.close()
-
-#       Я уже и забыл, что за человек это придумал, но спасибо ему
-#       А забыл я его, потому что изначально он сделал всё крайне непонятно
-        random_range = string.ascii_uppercase + string.digits
-        random_string = random.choices(random_range, k=69)
-        origin_string = ''.join(random_string)
-
-        lone_hex = bytes.fromhex('01')
-        strange_code = '6a8tf0Meh6T4x7b0XvwEt+Xw6k8='
-        strange_decode = base64.b64decode(strange_code)
-        string_code = sha1(origin_string.encode("utf-8"))
-        string_hex = string_code.hexdigest()
-        complex_code = sha1(lone_hex + string_code.digest() + strange_decode)
-        complex_hex = complex_code.hexdigest()
-
-        new_id = '01' + string_hex + complex_hex
-
-        info['device-id'] = new_id
-
-        login_info = open('device.json', 'w')
-        json.dump(info, login_info)
-        login_info.close()
-
-        return ''.join('Амино обнаружило, что используется кастомный клиент,',
-                       'но мы уже подделали для вас паспорт,',
-                       'попробуйте войти ещё раз')
-
 
 @handle_errors
 def get_communities():
-    communities = []
+    communities = {}
     comms = user.sub_clients()
 
     for i in range(0, len(comms.name)):
-        communities.append([comms.name[i], comms.comId[i]])
+        communities[comms.name[i]] = comms.comId[i]
 
     return communities
 
@@ -129,31 +67,30 @@ def enter_community(com_id):
     sub_user = amino.SubClient(comId=com_id, profile=user.profile)
     chats = sub_user.get_chat_threads()
 
-    ret_chats = []
+    ret_chats = {}
 
     for i in range(0, len(chats.title)):
         if chats.title[i] is None:
-            ret_chats.append(['Безымянный чач', chats.chatId[i]])
+            ret_chats['Безымянный чач ' + str(i)] = chats.chatId[i]
 
         else:
-            ret_chats.append([chats.title[i], chats.chatId[i]])
+            ret_chats[chats.title[i]] = chats.chatId[i]
 
     return ret_chats
 
 
 @handle_errors
-def get_messages(chat_id, count, last=[]):
-    global sub_user
+def get_messages(chat_id, count):
     non_text = 'НЕ ТЕКСТОВОЕ СООБЩЕНИЕ ТИПА '
 
     new_messages = sub_user.get_chat_messages(chat_id, count)
 
-    if count != len(new_messages.content):
-        count = len(new_messages.content) - 1
+    '''if count != len(new_messages.content):
+        count = len(new_messages.content) - 1'''
 
     current_iteration = []
 
-    for t in range(count):
+    for t in range(count - 1):
         first_part = new_messages.author.nickname[t] + ': '
 
         if new_messages.content[t] is None:
@@ -165,34 +102,15 @@ def get_messages(chat_id, count, last=[]):
         current_iteration.append([full_message,
                                   new_messages.author.userId[t],
                                   new_messages.messageId[t],
-                                  new_messages.type[t]
-                                  ])
+                                  new_messages.type[t]])
 
     current_iteration.reverse()
-
-    for message in current_iteration:
-        if not (message in last):
-            message_queue.put(message[0])
 
     return current_iteration
 
 
 @handle_errors
-def check_messages(chat_id):
-    last_iteration = get_messages(chat_id, 25)
-
-    while threading.main_thread().is_alive():
-        current_iteration = get_messages(chat_id, 4, last_iteration)
-
-        lumusServer.spam_check(current_iteration, chat_id, sub_user)
-
-        last_iteration = current_iteration
-
-
-@handle_errors
 def return_message(chat_id):
-    listner = threading.Thread(target=check_messages, args=[chat_id])
-    listner.start()
 
     repr_file = open('chat_repr.txt', 'w', encoding='utf-8')
 
@@ -209,7 +127,6 @@ def return_message(chat_id):
 @handle_errors
 def send_message(chat_id, message=None, tk_var=None, tk_error=None):
     try:
-        global sub_user
         send_ready = ''
         if tk_var:
             send_ready = tk_var.get()
--- desktop.py
+++ desktop.py
@@ -1,6 +1,7 @@
 from PyQt6 import QtWidgets
 import core
 import os
+import threading
 
 app = QtWidgets.QApplication([])
 
@@ -8,44 +9,142 @@ app = QtWidgets.QApplication([])
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+
         screen_size = app.screens()[0].availableGeometry()
         self.setGeometry(int(screen_size.width() / 4),
                          int(screen_size.height() / 4),
                          int(screen_size.width() / 2),
                          int(screen_size.height() / 2))
+        self.setFixedSize(self.width(), self.height())
+
         self.setWindowTitle('Nulla Client')
+
+
+class ChatContainer(QtWidgets.QWidget):
+    def __init__(self, parent, chat_id):
+        super().__init__(parent)
+        self.parent = parent
+        self.last = []
+        self.chat_id = chat_id
+
+        self.messages_list = QtWidgets.QListWidget(self)
+        input_field = QtWidgets.QTextEdit(self)
+        send_button = QtWidgets.QPushButton('Отправить', self)
+
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.messages_list)
+        layout.addWidget(input_field)
+        layout.addWidget(send_button)
+        self.setLayout(layout)
+
+        self.setGeometry(int(parent.width() / 3),
+                         0,
+                         int(parent.width() * 2 / 3),
+                         int(parent.height()))
+
+        chat_thread = threading.Thread(target=self.get_message)
+        chat_thread.start()
+
+    def get_message(self):
+        while True:
+            last_message = core.get_messages(self.chat_id, 1)
+            if last_message != self.last:
+                self.messages_list.addItem(last_message[0])
+                self.last = last_message
+
+
+class ChatsList(QtWidgets.QListWidget):
+    def __init__(self, parent, chats):
+        super().__init__(parent)
+        self.parent = parent
+        self.chats = chats
+
+        self.setGeometry(int(parent.width() / 3),
+                         0,
+                         int(parent.width() / 3),
+                         int(parent.height()))
+
+        self.itemClicked.connect(self.enter_chat)
+
+    def update(self, chats):
+        self.clear()
+        self.addItems(chats.keys())
+        self.chats = chats
+
+    def enter_chat(self):
+        selected_chat = self.selectedItems()[0].text()
+        chat_id = self.chats[selected_chat]
+        chat = ChatContainer(self.parent, chat_id)
+        chat.show()
+        self.hide()
+
+
+class CommunitiesList(QtWidgets.QListWidget):
+    def __init__(self, parent, communities):
+        super().__init__(parent)
+        self.parent = parent
+        self.communities = communities
+
+        self.chats_list = ChatsList(self.parent, [])
+        self.chats_list.hide()
+
+        self.addItems(list(communities.keys()))
+
+        self.itemClicked.connect(self.enter_community)
+
+        self.setGeometry(0,
+                         0,
+                         int(parent.width() / 3),
+                         int(parent.height()))
+
+    def enter_community(self):
+        community_name = self.selectedItems()[0].text()
+        community_id = self.communities[community_name]
+        chats = core.enter_community(community_id)
+        self.chats_list.update(chats)
+        self.chats_list.show()
 
 
 class LoginForm(QtWidgets.QWidget):
     def __init__(self, parent):
         super().__init__(parent)
+        self.parent = parent
+
         login_input = QtWidgets.QLineEdit(self)
         password_input = QtWidgets.QLineEdit(self)
+        accept_button = QtWidgets.QPushButton('Войти', self)
+
         layout = QtWidgets.QGridLayout(self)
-        layout.addWidget(login_input, 0, 1, 0, 1)
-        layout.addWidget(password_input, 1, 1, 2, 2)
+        layout.addWidget(login_input, 0, 0)
+        layout.addWidget(password_input, 1, 0)
+        layout.addWidget(accept_button, 3, 0)
         self.setLayout(layout)
 
+        self.setGeometry(int(parent.width() / 3),
+                         0,
+                         int(parent.width() / 3),
+                         int(parent.height()))
 
-'''
-login_container = QtWidgets.QWidget(window)
-login_container.setGeometry(0,
-                            0,
-                            window.size().width(),
-                            window.size().height())
-login_container.setGeometry
-layout = QtWidgets.QGridLayout(login_container)
+        accept_button.pressed.connect(lambda: self.login(login_input,
+                                                         password_input))
 
-login_label = gui.SelfShowingLineEdit(layout, 0, 0, login_container)
-login_label.show()'''
+    def login(self, login_input, password_input):
+        status = core.login(login_input.text(), password_input.text())
+        if status == '200':
+            communities = core.get_communities()
+            communities_list = CommunitiesList(self.parent, communities)
+            communities_list.show()
+            self.hide()
+        else:
+            print(status)
 
 
 if __name__ == '__main__':
     main_window = MainWindow()
     login_form = LoginForm(main_window)
+
     main_window.show()
     app.exec()
 
-    print('Выход (закрытие окна)')
     core.logout()
     os._exit(0)
